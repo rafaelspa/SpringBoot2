67619878bcd4fb2ced928c69acfcc6f2baa7ed0b
rafaelspa <rsdepaula67@gmail.com>
2022-05-18 14:34:42 -0300

    RequestParam

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AnimeRepository extends JpaRepository<Anime, Long> {}
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+
+    List<Anime> findByName(String name);
+}
